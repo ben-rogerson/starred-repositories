70be05146c0f6d41d6b973d5fb1e28f073416ccb
Ben Rogerson <info@benrogerson.com.au>
2018-06-14 20:42:08 +0930

    add lambda gh check function

---
+++ src/lambda/check.js
@@ -0,0 +1,55 @@
+const rp = require('request-promise')
+const cheerio = require('cheerio')
+
+const CHECK_INTERVAL = 5000
+
+const triggerBuild = () => {
+
+  const options = {
+    uri: `https://api.netlify.com/build_hooks/5b21957b0733d53a232b842a`,
+    method: 'POST',
+    body: '',
+  }
+
+  rp(options)
+    .then(() => {
+      console.log('fired off build url')
+    })
+    .catch(err => {
+      console.log(err)
+    })
+
+}
+
+exports.handler = async () => {
+
+  let number = 0
+
+  const options = {
+    uri: `https://github.com/ben-rogerson?tab=stars`,
+    transform: body => cheerio.load(body)
+  }
+
+  setInterval(() => {
+
+    rp(options)
+      .then($ => {
+        const startNumber = $('.UnderlineNav-item.selected .Counter').text().trim()
+        console.log(startNumber)
+
+        if (number != 0 && number != startNumber) {
+          triggerBuild()
+        }
+        number = startNumber
+      })
+      .catch(err => {
+        console.error(
+          (err.statusCode === 429) ?
+            'error - github abuse' :
+            err
+        )
+      })
+
+  }, CHECK_INTERVAL)
+
+}
