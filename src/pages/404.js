79f799efe3ac5ce974efadebb2090c8c3cf6838e
Ben Rogerson <info@benrogerson.com.au>
2019-02-16 16:35:33 +1030

    fix error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  <div className="page">
+    <main className="welcome">
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </main>
   </div>
 )
 
